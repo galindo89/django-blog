300854f87ddd5742c8a8b9b68f626273030a8c1f
Pablo <pablo@livedooh.com>
2024-12-09 00:55:29 +0100

    adding views and templates

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,23 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+   
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title} by {self.author}"  # Use the title as the representation
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return f"Comment {self.post} by {self.author}"  # Use the title as the representation
+    
+    class Meta:
+        ordering = ["created_on"]
+    
