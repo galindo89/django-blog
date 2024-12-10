ed062444495e6b8be8eac3b31c9b75f48fe42806
Pablo <pablo@livedooh.com>
2024-12-10 01:27:50 +0100

    adding CSS and Templates. Adding static file handling for Heroku

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     model=Post
-    queryset=Post.objects.all().filter(status=0)
+    queryset=Post.objects.all().filter(status=1)
     template_name='blog/index.html'
     paginate_by=6
     
--- codestar/settings.py
+++ codestar/settings.py
@@ -21,7 +21,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,12 +67,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whiteNoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
 ]
 
 ROOT_URLCONF = 'codestar.urls'
@@ -152,6 +154,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
