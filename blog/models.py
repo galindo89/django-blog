1073dc82610ad64f58e1201f2377197b3cd66d98
Pablo <pablo@livedooh.com>
2024-12-17 04:51:35 +0100

    adding integration with cloudinary

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -8,6 +9,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author= models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
--- codestar/settings.py
+++ codestar/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "cloudinary_storage",
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -68,6 +69,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5", 
     "django_summernote",
+    "cloudinary",
     'blog',
     "about",    
    
